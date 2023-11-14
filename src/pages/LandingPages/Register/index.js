84c51f2253fc285b270d7197ed0602f6969392da
steplove~ <loveaumak@gmail.com>
2023-11-14 10:31:27 +0700

    แก้alert

--- src/pages/LandingPages/Register/index.js
+++ src/pages/LandingPages/Register/index.js
@@ -205,7 +205,7 @@ function Register() {
                 if (response.status === 200) {
                   // แสดง sweetalert2 เพื่อแจ้งเตือนว่าเพิ่มข้อมูลพนักงานสำเร็จ
                   Swal.fire({
-                    title: "ลงทะเบียนสำเร็จสำเร็จ!",
+                    title: "ลงทะเบียนสำเร็จ!",
                     icon: "success",
                     showConfirmButton: false,
                     timer: 1500,
