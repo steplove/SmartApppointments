021c8a9e38e6755efa235be443db335eef708b7c
steplove <47933243+steplove@users.noreply.github.com>
2024-02-02 11:08:03 +0700

    lineliff

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "App";
 import reportWebVitals from "./reportWebVitals";
-// import liff from "@line/liff";
+import liff from "@line/liff";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// liff.init({ liffId: "2000414649-29Q1LMp4" }).then(() => {
-//   // ตรวจสอบสถานะการเข้าสู่ระบบ
-//   if (!liff.isLoggedIn()) {
-//     liff.login();
-//   }
-// });
+liff.init({ liffId: "2000414649-29Q1LMp4" }).then(() => {
+  // ตรวจสอบสถานะการเข้าสู่ระบบ
+  if (!liff.isLoggedIn()) {
+    liff.login();
+  }
+});
 root.render(
   <BrowserRouter>
     <App />
